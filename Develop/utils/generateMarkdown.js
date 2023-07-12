1fc5147b36c88c4a73640bf1d0057485ed19876c
Faith Fechser <faithfechser@gmail.com>
2023-07-12 10:42:48 -0600

    Readme generation layout function created, license functions created

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,68 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// License Badge Return
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// License Link Return
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `- [License](#license)`;
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// License Return
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
 
-// TODO: Create a function to generate markdown for README
+This application is covered under the ${license} license.`;
+  }
+  return '';
+}
+
+// ReadMe Markdown Gen Function
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Contributions](#contributions)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ${renderLicenseLink(data.license)}
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  
+  ## Contributions
+  
+  ${data.contributions}
+  
+  ## Tests
+  
+  ${data.tests}
+  
+  ## Questions
+  
+  For any questions, feel free to contact me:
+  
+  GitHub: [${data.github}](https://github.com/${data.github})
+  
+  Email: ${data.email}
+  
+  ${renderLicenseSection(data.license)}
 `;
 }
 
