8e74c4fed929f4edd1b1fd74b59fe862823d6ceb
Faith Fechser <faithfechser@gmail.com>
2023-07-12 09:12:27 -0600

    add first questions and functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,47 @@
-// TODO: Include packages needed for this application
+// Application Packages
+const path = require('path')
+const fs = require('fs')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
+// User Questions
+const questions = [{
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  }
+];
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// ReadMe Function
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// App Initialization Function
+function init() {
+    inquirer
+  .prompt(
+    // Question prompting
+  questions)
+  .then((answers) => {
+    // Answers
+    console.log(answers) 
+    writeToFile('README.md', generateMarkdown({ ...answers }))
+  })
+  .catch((error) => {
+    if (error.isTtyError) { console.log(error)
+      // Prompt Rendering Environment Issues
+    } else {
+        console.log('somethingelse')
+    }
+  });
+    // inquirer.prompt(questions).then((data) => {console.log('generating')})
+    // writeToFile('README.md', generateMarkdown({ ...data })) 
+}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
+// App Initialization Function Call
 init();
