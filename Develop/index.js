095f0f85c178b7a2366363d5856e9e804119ad91
Faith Fechser <faithfechser@gmail.com>
2023-07-12 10:40:55 -0600

    cleaned up App Init. function

--- Develop/index.js
+++ Develop/index.js
@@ -54,25 +54,15 @@ function writeToFile(fileName, data) {
 
 // App Initialization Function
 function init() {
-    inquirer
-  .prompt(
-    // Question prompting
-  questions)
-  .then((answers) => {
-    // Answers
-    console.log(answers) 
-    writeToFile('README.md', generateMarkdown({ ...answers }))
-  })
-  .catch((error) => {
-    if (error.isTtyError) { console.log(error)
-      // Prompt Rendering Environment Issues
-    } else {
-        console.log('somethingelse')
+    inquirer.prompt(questions)
+        .then(answers => {
+        writeToFile('README.md', generateMarkdown(answers));
+        console.log('README.md generated successfully!');
+        })
+        .catch(error => {
+        console.log(error);
+        });
     }
-  });
-    // inquirer.prompt(questions).then((data) => {console.log('generating')})
-    // writeToFile('README.md', generateMarkdown({ ...data })) 
-}
 
 // App Initialization Function Call
 init();
